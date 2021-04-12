c7f1360bc87e46d958230569ffec929de782c74a
Frederic Lavigne <frederic.lavigne@ibm.com>
2021-04-12 18:00:51 -0500

    create namespace

--- terraform/main.tf
+++ terraform/main.tf
@@ -26,6 +26,12 @@ variable "tags" {
   default = ["terraform"]
 }
 
+variable "registry_namespace_name" {
+  type        = string
+  default     = ""
+  description = "Name of the IBM Cloud Container registry namespace."
+}
+
 terraform {
   required_version = ">=0.13"
   required_providers {
@@ -53,4 +59,32 @@ resource "ibm_resource_group" "group" {
 data "ibm_resource_group" "group" {
   count = var.resource-group != "" ? 1 : 0
   name  = var.resource-group
+}
+
+resource "ibm_cr_namespace" "namespace" {
+  count             = var.registry_namespace_name != "" ? 0 : 1
+  name              = "${var.resource-prefix}-cr-namespace"
+  resource_group_id = local.resource_group_id
+}
+
+locals {
+  resource_group_id       = var.resource-group != "" ? data.ibm_resource_group.group.0.id : ibm_resource_group.group.0.id
+  resource_group_name     = var.resource-group != "" ? data.ibm_resource_group.group.0.name : ibm_resource_group.group.0.name
+  registry_namespace_name = var.registry_namespace_name != "" ? var.registry_namespace_name : ibm_cr_namespace.namespace.0.name
+}
+
+output "resource_group_id" {
+  value = local.resource_group_id
+}
+
+output "region" {
+  value = var.region
+}
+
+output "resource_group_name" {
+  value = local.resource_group_name
+}
+
+output "registry_namespace" {
+  value = local.registry_namespace_name
 }
